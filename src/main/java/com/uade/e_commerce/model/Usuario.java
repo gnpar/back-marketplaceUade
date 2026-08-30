aa4653385a963dc2c235b0dfab7c5835aa296634
Carina Milano <carinamilano@gmail.com>
2026-08-29 23:10:35 -0300

    Agrego entidad Usuario (sin relaciones aún)

---
+++ src/main/java/com/uade/e_commerce/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.uade.e_commerce.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombreUsuario;
+    private String mail;
+    private String contrasena;
+    private String nombre;
+    private String apellido;
+    
+}
