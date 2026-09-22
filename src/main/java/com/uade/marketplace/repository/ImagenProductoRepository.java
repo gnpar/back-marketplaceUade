e29b00ac563aa0ad57752fbc906263fe0ac53496
Carina Milano <carinamilano@gmail.com>
2026-09-22 19:03:39 -0300

    refactor: quito @Repository innecesario en ImagenProductoRepository

--- src/main/java/com/uade/marketplace/repository/ImagenProductoRepository.java
+++ src/main/java/com/uade/marketplace/repository/ImagenProductoRepository.java
@@ -4,9 +4,7 @@ import com.uade.marketplace.model.ImagenProducto;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ImagenProductoRepository extends JpaRepository<ImagenProducto, Long> {
     List<ImagenProducto> findByProductoIdOrderByIdAsc(Long productoId);
 
